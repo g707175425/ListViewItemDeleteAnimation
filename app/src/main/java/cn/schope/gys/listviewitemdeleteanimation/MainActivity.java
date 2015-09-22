549de313355778a02bb2fa7b33571bd0dd046f54
gys <707175425@qq.com>
2015-09-22 14:11:02 +0800

    MainActivity

--- app/src/main/java/cn/schope/gys/listviewitemdeleteanimation/MainActivity.java
+++ app/src/main/java/cn/schope/gys/listviewitemdeleteanimation/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity implements OnDismissCallback {
     private MyAdapter adapter;
 
     {
-        for (int i = 0;i<20;i++){
+        for (int i = 0;i<10;i++){
             strs.add("我是第:"+i+"个Item");
         }
     }
